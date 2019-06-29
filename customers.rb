cff2e408d6cd5333e338b2797af1d9c6ad6f713e
Richard Peck <rpeck@frontlineutilities.co.uk>
2019-06-29 20:13:46 +0100

    Inspect

--- customers.rb
+++ customers.rb
@@ -58,7 +58,8 @@ error Bigcommerce::BadRequest do
   message = JSON.parse(env['sinatra.error'].message.to_s)
   test = OpenStruct.new message.first
   #test.status.to_s
-  test.message
+  #test.message
+  test.details.inspect()
 end
 
 ##########################################################
