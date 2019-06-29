4024dda9179da022b3028e0df858ac12fee39391
Richard Peck <rpeck@frontlineutilities.co.uk>
2019-06-29 17:15:13 +0100

    Added

--- customers.rb
+++ customers.rb
@@ -53,7 +53,7 @@ end
 ## Exception Handling ##
 error Bigcommerce::BadRequest do
   message = JSON.parse env['sinatra.error'].message
-  message[:status]
+  message.inspect()
 end
 
 ##########################################################
