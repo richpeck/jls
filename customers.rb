4efe57651424ad6c3d988ab68562149cb6503a47
Richard Peck <rpeck@frontlineutilities.co.uk>
2019-06-30 12:21:11 +0100

    Invalid Reason

--- customers.rb
+++ customers.rb
@@ -78,7 +78,7 @@ error Bigcommerce::BadRequest do
 
   ## Response ##
   respond_to do |wants|
-    wants.html { message.inspect() } # => bare message
+    wants.html { message["details"]["invalid_reason"] } # => bare message
     wants.js   { message.inspect() } # => bare message
   end
 
